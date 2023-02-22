3798820e8a8b9be7b59e9aa7cf7670295c6a631a
ljb2458 <ljb2458@outlook.com>
2023-02-22 23:04:13 +0800

    重置了rpx

--- src/components/base/PageView.vue
+++ src/components/base/PageView.vue
@@ -1,17 +1,31 @@
+<!--
+ * @Date: 2023-02-21 22:32:00
+ * @LastEditTime: 2023-02-22 22:46:17
+ * @FilePath: /my-vue3-project/src/components/base/PageView.vue
+ * 介绍:
+-->
 <script lang="ts" setup>
-import { uniGetSystemInfo } from "@@/hooks/rewriteUni";
-const rootFontSize = ref(1);
-async function resetRem() {
-  const res = await uniGetSystemInfo();
-  console.log(res.screenWidth);
-
-  rootFontSize.value = res.screenWidth / 375;
-}
-resetRem();
+const porps = defineProps<{
+  /**开启下拉刷新吗 */
+  usePullDownRefresh?: boolean;
+}>();
+const emit = defineEmits<{
+  (e: "resize", v: any): void;
+  (e: "scroll", v: any): void;
+  (e: "scrolldone", v: any): void;
+}>();
+const rootFontSize = uni.upx2px(1) + "px";
 </script>
 
 <template>
-  <page-meta :root-font-size="rootFontSize + 'px'" class="PageView">
+  <page-meta
+    :root-font-size="rootFontSize"
+    :enable-pull-down-refresh="porps.usePullDownRefresh || false"
+    @resize="(e:any)=>emit('resize',e)"
+    @scroll="(e:any)=>emit('scroll',e)"
+    @scrolldone="(e:any)=>emit('scrolldone',e)"
+    class="PageView"
+  >
     <slot></slot>
   </page-meta>
 </template>
