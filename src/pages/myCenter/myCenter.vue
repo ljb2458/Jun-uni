4a08d30b99b80cef57d93f5412c7831cedbaffea
ljb2458 <ljb2458@outlook.com>
2023-06-14 19:50:23 +0800

    页面增加

---
+++ src/pages/myCenter/myCenter.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Date: 2023-06-14 19:49:30
+ * @LastEditTime: 2023-06-14 19:49:47
+ * @FilePath: /music-client/src/pages/myCenter/myCenter.vue
+ * 介绍:
+-->
+<cfg lang="json">
+{
+  "title": "个人中心",
+  "enablePullDownRefresh": false
+}
+</cfg>
+<script lang="ts" setup>
+// code...
+</script>
+<template>
+  <CpageView> </CpageView>
+</template>
+<style lang="scss" scoped></style>
