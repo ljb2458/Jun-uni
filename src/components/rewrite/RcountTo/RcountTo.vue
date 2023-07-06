d3dcc0fe11dca47d1f8439509e25a138286957ee
ljb2458 <ljb2458@outlook.com>
2023-07-06 15:51:24 +0800

    修改默认值

--- src/components/rewrite/RcountTo/RcountTo.vue
+++ src/components/rewrite/RcountTo/RcountTo.vue
@@ -1,28 +1,34 @@
 <script lang="ts" setup>
 import { filterObject } from "@@/utils/tools/object";
 
-const props = defineProps<{
-  /**开始值 */
-  startVal: StrNumber;
-  /**结束值 */
-  endVal: StrNumber;
-  /**滚动过程所需的时间，单位ms */
-  duration: StrNumber;
-  /**是否自动开始滚动 */
-  autoplay: boolean;
-  /**要显示的小数位数，见上方说明 */
-  decimals: StrNumber;
-  /**滚动结束时，是否缓动结尾，见上方说明 */
-  useEasing: boolean;
-  /**十进制分割 */
-  decimal: string;
-  color: string;
-  fontSize: StrNumber;
-  /**字体是否加粗 */
-  bold: boolean;
-  /**千位分隔符 */
-  separator: string;
-}>();
+const props = withDefaults(
+  defineProps<{
+    /**开始值 */
+    startVal: StrNumber;
+    /**结束值 */
+    endVal: StrNumber;
+    /**滚动过程所需的时间，单位ms */
+    duration: StrNumber;
+    /**是否自动开始滚动 */
+    autoplay: boolean;
+    /**要显示的小数位数，见上方说明 */
+    decimals: StrNumber;
+    /**滚动结束时，是否缓动结尾，见上方说明 */
+    useEasing: boolean;
+    /**十进制分割 */
+    decimal: string;
+    color: string;
+    fontSize: StrNumber;
+    /**字体是否加粗 */
+    bold: boolean;
+    /**千位分隔符 */
+    separator: string;
+  }>(),
+  {
+    autoplay: true,
+    useEasing: true,
+  }
+);
 const emit = defineEmits<{
   (e: "end"): void;
 }>();
