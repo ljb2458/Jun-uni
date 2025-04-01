232d92b12e8a11af9c8423a754f3eceddae08801
ljb2458 <ljb2458@outlook.com>
2025-04-01 18:05:23 +0800

    #增加简单的地图demo

--- src/pages/demo/CoMap.vue
+++ src/pages/demo/CoMap.vue
@@ -12,8 +12,40 @@ import { CoMapMapProps } from "@/components/common/CoMap/CoMap.vue";
 import { onPageScroll } from "@dcloudio/uni-app";
 
 const mapProps = computed(() => {
-  const props: CoMapMapProps = {};
-  return props
+  const latitude = 39.909;
+  const longitude = 116.39742;
+  const address = "北京市天安门广场内";
+  function markertap(id: number) {
+    if (id === 1) {
+      uni.openLocation({
+        latitude,
+        longitude,
+        name: address,
+      });
+    }
+  }
+  const props: CoMapMapProps = {
+    latitude,
+    longitude,
+    markers: [
+      {
+        id: 1,
+        latitude,
+        longitude,
+        address,
+        customCallout: {
+          display: "ALWAYS",
+        },
+      },
+    ],
+    onCallouttap(e) {
+      markertap(e.detail.markerId);
+    },
+    onMarkertap(e) {
+      markertap(e.detail.markerId);
+    },
+  };
+  return props;
 });
 </script>
 
@@ -21,8 +53,25 @@ const mapProps = computed(() => {
   <CoPageView class="B-B2 PB-md" :onPageScroll="onPageScroll">
     <view class="PD-md">
       <view class="MT-md F-S-lg">基本使用</view>
-      <view class="C-T3">建议使用微信小程序查看</view>
-      <CoMap :mapProps="mapProps" style="height: 300px;"></CoMap>
+      <view class="C-T3 MB-xs">建议使用微信小程序查看</view>
+      <CoMap :mapProps="mapProps" style="height: 300px">
+        <template #callout="{ markers }">
+          <cover-view
+            style="width: 60vw"
+            class="flex-A-STR B-B1 R-sm"
+            v-for="item in markers"
+            :key="item.id"
+            :marker-id="item.id"
+          >
+            <cover-view class="flex-1 PD-xs">
+              {{ item.address }}
+            </cover-view>
+            <cover-view class="flex-J-C flex-A-C PD-xs B-M1 C-white">
+              <cover-view>导航</cover-view>
+            </cover-view>
+          </cover-view>
+        </template>
+      </CoMap>
     </view>
   </CoPageView>
 </template>
