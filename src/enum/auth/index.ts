bc6934c253e3be998beecca4024d68f6532fd007
ljb2458 <ljb2458@outlook.com>
2025-08-25 11:22:30 +0800

    删除多余文件

--- src/enum/auth/index.ts
+++ src/enum/auth/index.ts
@@ -1,4 +1,2 @@
 /**登录白名单 */
 export const LOGIN_WHITE_LIST: RegExp[] = [/^\/pages\/tabbar/];
-/**登录页 */
-export const LOGIN_PATH = "@/pages/system/login.vue";
--- src/enum/dictOrm/index.ts
+++
@@ -1,6 +0,0 @@
-import useSysStore from "@/store/useSysStore";
-
-export const dictOrm = computed(() => {
-  const sysStore = useSysStore();
-  return sysStore.dictOrm;
-});
